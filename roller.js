1ce61d636a11eb0dd11c9237fb750d0c4df193d4
Niko <niko.kettunen@reaktor.fi>
2014-08-27 00:07:53 +0300

    Sketch out

--- roller.js
+++ roller.js
@@ -2,16 +2,31 @@
 // @name       imgur roller
 // @namespace  http://use.i.E.your.homepage/
 // @version    0.1
-// @description  enter something useful
-// @match      http://imgur.com/r/*/*
-// @copyright  2012+, You
+// @description  Full screen imgur rolling
+// @match      http://imgur.com/r/*/
+// @copyright  2014+, kopzu
 // @require
 // ==/UserScript==
 
 
-$.get("hit.json", function (response) {
-  var hashes = _.map(response.data, function (img) {
-    return img.hash
+showImagesFromPage(0)
+
+function showImagesFromPage(pageNumber) {
+  $.get('page/' + pageNumber + '/hit.json', function (response) {
+    var data = response.data
+
+    if (data.length > 0) {
+      var hashes = _.each(response.data, function (img) {
+        return img.hash
+        //render with timeout
+        // load image smoothly
+
+      })
+      // call showImagesFromPage pageNumber+1
+    } else if (pageNumber > 0) {
+      showImagesFromPage(0)
+    } else {
+      // no content, load funny pic from imgur
+    }
   })
-  console.log(hashes)
-})
+}
