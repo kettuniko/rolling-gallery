bb5d4d6edb7a2145e098c3a11fd4feb5dde7e824
Niko Kettunen <niko.kettunen@reaktor.fi>
2017-08-06 16:47:40 +0300

    Add release script

---
+++ release.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+git fetch --all
+git checkout gh-pages
+git reset --hard origin/master
+yarn build
+git add --force dist
+git commit -m 'Release'
+git push --force
+git checkout -
