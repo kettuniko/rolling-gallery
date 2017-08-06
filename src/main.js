f9fb86f8f410e3e0d25d0191054e1d757b48823b
Niko Kettunen <niko.kettunen@reaktor.fi>
2017-08-06 19:18:40 +0300

    Import functions in place of whole modules

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import queryString from 'query-string'
-import * as R from 'ramda'
+import { parse } from 'query-string'
+import { compose, ifElse, isNil, prop } from 'ramda'
 import frontPage from './front-page'
 import slideShow from './slide-show'
 
 window.onerror = errorMsg => document.body.innerHTML = errorMsg
 
-const init = R.compose(
-  R.ifElse(R.isNil, frontPage, slideShow),
-  R.prop('r'),
-  queryString.parse
+const init = compose(
+  ifElse(isNil, frontPage, slideShow),
+  prop('r'),
+  parse
 )
 
 init(window.location.search)
