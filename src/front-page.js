da8de4192bf51df4e4d72dd638199b57056a15f8
Niko Kettunen <niko.kettunen@reaktor.fi>
2017-08-08 22:24:55 +0300

    Refactor front-page.js

--- src/front-page.js
+++ src/front-page.js
@@ -1,17 +1,21 @@
+import { compose, map, reduce } from 'ramda'
 import fetchGallery from './fetch-gallery'
 import toDomNode from './to-dom-node'
 
 const galleries = ['brokengifs', 'wastedgifs', 'earthporn', 'reactiongifs', 'gifs', 'wallpapers', 'woahdude', 'perfecttiming', 'foodporn', 'unexpected']
 
-export default () => galleries
-  .map((g) => fetchGallery(g))
-  .reduce((sequence, current) => sequence
-    .then(() => current
-      .then(([{ id, section }]) =>
-        toDomNode(`
-          <a class="gallery-head" href="?r=${section}">
-            <img src="${`https://i.imgur.com/${id}b.jpg`}" />
-            <span class="gallery-head__name">${section}</span>
-          </a>`))
-      .then(img => document.querySelector('.galleries').appendChild(img))), Promise.resolve())
-  .then(() => document.querySelector('.loading').remove())
+const renderSequentially = (sequence, current) => sequence
+  .then(() => current
+    .then(([{ id, section }]) =>
+      toDomNode(`
+        <a class="gallery-head" href="?r=${section}">
+          <img src="${`https://i.imgur.com/${id}b.jpg`}" />
+          <span class="gallery-head__name">${section}</span>
+        </a>`))
+    .then(img => document.querySelector('.galleries').appendChild(img)))
+
+export default () => compose(
+  completed => completed.then(() => document.querySelector('.loading').remove()),
+  reduce(renderSequentially, Promise.resolve()),
+  map(fetchGallery))
+(galleries)
