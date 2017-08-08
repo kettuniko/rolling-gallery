6fd74d57f46ca0d2eb9733bb37f81d4309cb9bfc
Niko Kettunen <niko.kettunen@reaktor.fi>
2017-08-08 22:10:06 +0300

    Refactor fetch-gallery

--- src/fetch-gallery.js
+++ src/fetch-gallery.js
@@ -1,11 +1,12 @@
+import { compose, contains, filter, path, prop } from 'ramda'
 import { get } from 'axios'
 
-const isDisplayAble = ({ link }) => link.includes('i.imgur.com')
+const isDisplayAble = compose(contains('i.imgur.com'), prop('link'))
 
 export default (gallery, pageNumber = 0) =>
   get(`https://api.imgur.com/3/gallery/r/${gallery}/time/${pageNumber}`, {
     headers: {
       Authorization: 'Client-ID 1400c78269df7bc'
     }
-  }).then(responseBody => responseBody.data.data)
-    .then(images => images.filter(isDisplayAble))
+  }).then(path(['data', 'data']))
+    .then(filter(isDisplayAble))
