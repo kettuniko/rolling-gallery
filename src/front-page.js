127672b649efe81d47a53dc46f915601bc06fdc7
Niko Kettunen <niko.kettunen@reaktor.fi>
2017-08-11 16:10:07 +0300

    Rewrite front-page with react

--- src/front-page.js
+++ src/front-page.js
@@ -1,6 +1,7 @@
-import { map, pipe, reduce } from 'ramda'
+import { append, head, map, pick } from 'ramda'
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import fetchGallery from './fetch-gallery'
-import toDomNode from './to-dom-node'
 
 const galleries = [
   'brokengifs',
@@ -15,27 +16,50 @@ const galleries = [
   'unexpected'
 ]
 
-const renderSequentially = (sequence, current) => sequence
-  .then(() => current
-    .then(([{ id, section }]) =>
-      toDomNode(`
-        <a class="gallery-head" href="?r=${section}">
-          <img src="${`https://i.imgur.com/${id}b.jpg`}" />
-          <span class="gallery-head__name">${section}</span>
-        </a>`))
-    .then(img => document.querySelector('.galleries').appendChild(img)))
 
-export default () => {
-  const frontPage = toDomNode(`
-    <div class="galleries"></div>
-    <h1 class="loading">Loading...</h1>
-    <footer class="footer">image copyrights: <a href="https://imgur.com/">imgur.com</a></footer>
-  `)
-  document.querySelector('.root').appendChild(frontPage)
+const Gallery = ({ id, section }) =>
+  <a className="gallery-head" href={`?r=${section}`}>
+    <img src={`https://i.imgur.com/${id}b.jpg`}/>
+    <span className="gallery-head__name">{section}</span>
+  </a>
 
-  return pipe(
-    map(fetchGallery),
-    reduce(renderSequentially, Promise.resolve())
-  )(galleries)
-    .then(() => document.querySelector('.loading').remove());
+class FrontPage extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      fetching: true,
+      galleries: []
+    }
+  }
+
+  componentDidMount() {
+    const renderSequentially = (sequence, current) => sequence
+      .then(() => current
+        .then(head)
+        .then(pick(['id', 'section']))
+        .then(gallery =>
+          this.setState({ galleries: append(gallery, this.state.galleries) })
+        ))
+
+    galleries
+      .map(g => fetchGallery(g))
+      .reduce(renderSequentially, Promise.resolve())
+      .then(() => this.setState({ fetching: false }))
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="galleries">
+          {map(({ id, section }) => <Gallery id={id} section={section} key={id}/>, this.state.galleries)}
+        </div>
+        {this.state.fetching && <h1 className="loading">Loading...</h1>}
+        <footer className="footer">image copyrights: <a href="https://imgur.com/">imgur.com</a></footer>
+      </div>
+    )
+  }
 }
+
+export default () => ReactDOM.render(<FrontPage/>, document.querySelector('.root'))
+
