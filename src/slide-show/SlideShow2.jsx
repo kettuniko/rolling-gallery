1e540fb8db46353d9bd00a5a1add9974b339bd7e
Niko Kettunen <niko.kettunen@reaktor.fi>
2017-09-02 20:18:56 +0300

    Revoke objectUrl after onLoad

--- src/slide-show/SlideShow2.jsx
+++ src/slide-show/SlideShow2.jsx
@@ -1,11 +1,14 @@
 import './SlideShow.css'
 
 import { get } from 'axios'
-import { prop } from 'ramda'
+import { compose, path, prop } from 'ramda'
 import React, { Component } from 'react'
 import Spinner from '../spinner/Spinner.jsx'
 
 const fetchBlob = url => get(url, { responseType: 'blob' }).then(prop('data'))
+
+const { createObjectURL, revokeObjectURL } = window.URL
+const revokeOnLoad = compose(revokeObjectURL, path(['target', 'src']))
 
 export default class SlideShow extends Component {
   constructor(props) {
@@ -19,14 +22,14 @@ export default class SlideShow extends Component {
 
   componentDidMount() {
     fetchBlob('https://i.imgur.com/lgCsd14.jpg')
-      .then(window.URL.createObjectURL)
+      .then(createObjectURL)
       .then(imageUrl => this.setState({ imageUrl, fetching: false }))
   }
 
   render() {
     return (
       <div>
-        {this.state.imageUrl && <img src={this.state.imageUrl}/>}
+        {this.state.imageUrl && <img onLoad={revokeOnLoad} src={this.state.imageUrl}/>}
         {this.state.fetching && <div className='slide-show-spinner'><Spinner/></div>}
       </div>
     )
