9eccc328c0ae746e5fec37514667d988a6f5f6a9
Niko Kettunen <niko.kettunen@reaktor.fi>
2017-10-07 17:25:14 +0300

    Export named Spinner for better stack traces

--- src/spinner/Spinner.jsx
+++ src/spinner/Spinner.jsx
@@ -2,7 +2,11 @@ import './Spinner.css'
 import { times } from 'ramda'
 import React from 'react'
 
-export default () =>
-  <div className="spinner ispinner ispinner--gray ispinner--animating">
-    {times((key) => <div key={key} className="ispinner__blade"/>, 12)}
-  </div>
+export default function Spinner() {
+  return (
+    <div className="spinner ispinner ispinner--gray ispinner--animating">
+      {times((key) => <div key={key} className="ispinner__blade"/>, 12)}
+    </div>
+  )
+}
+
