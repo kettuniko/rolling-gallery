190a9499ad3bc6ece2efab921070ac0af9efb0ad
Niko Kettunen <niko.kettunen@reaktor.fi>
2017-10-07 17:23:14 +0300

    Export named Progressbar for better stack traces

--- src/progressbar/Progressbar.jsx
+++ src/progressbar/Progressbar.jsx
@@ -2,5 +2,8 @@ import './Progressbar.css'
 
 import React from 'react'
 
-export default ({ duration }) =>
-  <div className="progressbar" style={{ animationDuration: `${duration / 1000}s` }}/>
+export default function Progressbar({ duration }) {
+  return (
+    <div className="progressbar" style={{ animationDuration: `${duration / 1000}s` }}/>
+  )
+}
