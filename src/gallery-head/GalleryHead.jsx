7f2398a3d1713f9de89f5f5ba5dbf3e0aba6365b
Niko Kettunen <niko.kettunen@reaktor.fi>
2017-10-08 19:31:08 +0300

    Convert GalleryHead to React component

--- src/gallery-head/GalleryHead.jsx
+++ src/gallery-head/GalleryHead.jsx
@@ -1,11 +1,18 @@
 import './GalleryHead.css'
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function GalleryHead({ id, section }) {
-  return (
-    <a className="gallery-head" href={`?r=${section}`}>
-      <img className='gallery-head__image' src={`https://i.imgur.com/${id}b.jpg`}/>
-      <span className="gallery-head__name">{section}</span>
-    </a>
-  )
+export default class GalleryHead extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const { section, id } = this.props
+    return (
+      <a className="gallery-head" href={`?r=${section}`}>
+        <img className='gallery-head__image' src={`https://i.imgur.com/${id}b.jpg`}/>
+        <span className="gallery-head__name">{section}</span>
+      </a>
+    )
+  }
 }
