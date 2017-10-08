18aa4d52e1803ebbaaf26b88818131dbd1a32e30
Niko Kettunen <niko.kettunen@reaktor.fi>
2017-10-08 20:02:28 +0300

    Add initial fetching-spinner to GalleryHead

--- src/gallery-head/GalleryHead.jsx
+++ src/gallery-head/GalleryHead.jsx
@@ -1,15 +1,23 @@
 import './GalleryHead.css'
 import React, { Component } from 'react'
+import Spinner from '../spinner/Spinner.jsx'
 
 export default class GalleryHead extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      fetching: false
+    }
   }
 
   render() {
     const { section, id } = this.props
+    const { fetching } = this.state
+
     return (
       <a className="gallery-head" href={`?r=${section}`}>
+        {fetching && <span className='gallery-head__spinner'><Spinner /></span>}
         <img className='gallery-head__image' src={`https://i.imgur.com/${id}b.jpg`}/>
         <span className="gallery-head__name">{section}</span>
       </a>
