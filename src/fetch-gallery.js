49e23ebe883a9ab65697d4641c869e2678a0098c
Niko Kettunen <niko.kettunen@reaktor.fi>
2017-10-14 18:26:49 +0300

    Use fetchJson in fetch-gallery

--- src/fetch-gallery.js
+++ src/fetch-gallery.js
@@ -1,13 +1,14 @@
 import { compose, composeP, contains, curry, filter, path, prop } from 'ramda'
+import { fetchJson } from './fetch'
 
 const isDisplayAble = compose(contains('i.imgur.com'), prop('link'))
 
 const getGallery = (pageNumber, gallery) =>
-  window.fetch(`https://api.imgur.com/3/gallery/r/${gallery}/time/${pageNumber}`, {
+  fetchJson(`https://api.imgur.com/3/gallery/r/${gallery}/time/${pageNumber}`, {
     headers: {
       Authorization: 'Client-ID 1400c78269df7bc'
     }
-  }).then(response => response.json())
+  })
 
 export const fetchGallery = curry(
   composeP(
