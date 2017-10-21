99ee5df5d4b3363f0c5e267be8f44412ceaede01
Niko Kettunen <niko.kettunen@reaktor.fi>
2017-10-21 23:55:07 +0300

    Play preview video on hover

--- src/gallery-item/GalleryItem.jsx
+++ src/gallery-item/GalleryItem.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { __, always, equals, ifElse } from 'ramda'
 
-export default function GalleryItem(props) {
-  const { item: { animated, link, mp4 }, onLoad } = props
+export default class GalleryItem extends Component {
+  constructor(props) {
+    super(props)
+  }
 
-  return (
-    animated
-      ? <video className='gallery-item' src={mp4} onCanPlayThrough={onLoad} loop autoPlay muted playsInline/>
+  render() {
+    const { item: { animated, link, mp4 }, onLoad, autoPlay } = this.props
+
+    const whenAutoPlayDisabled = ifElse(equals(true), always, __, autoPlay)
+
+    return animated
+      ? <video className='gallery-item'
+               src={mp4}
+               onCanPlayThrough={onLoad}
+               ref={video => {this.video = video}}
+               onMouseOver={whenAutoPlayDisabled(() => this.video.play())}
+               onMouseOut={whenAutoPlayDisabled(() => this.video.pause())}
+               autoPlay={autoPlay}
+               loop
+               muted
+               playsInline/>
       : <img className='gallery-item' onLoad={onLoad} src={link}/>
-  )
+  }
 }
