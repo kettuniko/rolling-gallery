aa3d59cb43fded060786a7e35876eb3532797620
Niko Kettunen <niko.kettunen@reaktor.fi>
2017-10-22 19:51:32 +0300

    Play best item within viewport on small devices

--- src/gallery-item/GalleryItem.jsx
+++ src/gallery-item/GalleryItem.jsx
@@ -4,6 +4,31 @@ import { __, always, equals, ifElse } from 'ramda'
 export default class GalleryItem extends Component {
   constructor(props) {
     super(props)
+    this.playVideo = this.playVideo.bind(this)
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.playVideo)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.playVideo)
+  }
+
+  componentDidUpdate() {
+    this.playVideo()
+  }
+
+  playVideo() {
+    const video = this.video
+    if (video) {
+      const { bottom, top, width } = video.getBoundingClientRect()
+      const topInsideViewPort = top >= 0
+      const bottomInsideViewPort = bottom < window.outerHeight
+      const onlyHorizontalItem = width === window.outerWidth
+      const bestItemInViewPort = topInsideViewPort && bottomInsideViewPort && onlyHorizontalItem
+      bestItemInViewPort ? video.play() : video.pause()
+    }
   }
 
   render() {
@@ -15,7 +40,7 @@ export default class GalleryItem extends Component {
       ? <video className='gallery-item'
                src={mp4}
                onCanPlayThrough={onLoad}
-               ref={video => {this.video = video}}
+               ref={video => this.video = video}
                onMouseOver={whenAutoPlayDisabled(() => this.video.play())}
                onMouseOut={whenAutoPlayDisabled(() => this.video.pause())}
                autoPlay={autoPlay}
