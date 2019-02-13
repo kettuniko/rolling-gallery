e1a37ef8ffdd07968804e78d0b09d830593a74a4
Niko Kettunen <niko.kettunen@reaktor.com>
2019-02-13 20:20:55 +0200

    Don't show removed images

--- src/fetch-gallery.js
+++ src/fetch-gallery.js
@@ -1,7 +1,12 @@
-import { compose, composeP, contains, curry, filter, path, prop } from 'ramda'
-import { fetchJson } from './fetch'
+import { assoc, compose, composeP, contains, curry, F, filter, map, path, prop, T } from 'ramda'
+import { fetchHead, fetchJson } from './fetch'
 
-const isDisplayAble = compose(contains('i.imgur.com'), prop('link'))
+const isDisplayable = image => fetchHead(image.link)
+  .then(T)
+  .catch(F)
+  .then(isDisplayAble => assoc('isDisplayAble', isDisplayAble, image))
+
+const isImage = compose(contains('i.imgur.com'), prop('link'))
 
 const getGallery = (pageNumber, gallery) =>
   fetchJson(`https://api.imgur.com/3/gallery/r/${gallery}/time/${pageNumber}`, {
@@ -12,7 +17,10 @@ const getGallery = (pageNumber, gallery) =>
 
 export const fetchGallery = curry(
   composeP(
-    filter(isDisplayAble),
+    filter(prop('isDisplayAble')),
+    images => Promise.all(images),
+    map(isDisplayable),
+    filter(isImage),
     path(['data']),
     getGallery
   ))
--- src/fetch.js
+++ src/fetch.js
@@ -2,3 +2,4 @@ import { composeP, invoker, } from 'ramda'
 
 export const { fetch } = window
 export const fetchJson = composeP(invoker(0, 'json'), fetch)
+export const fetchHead = url => fetch(url, { method: 'HEAD', redirect: 'error' })
